9a77ede4f651bcf024097c0d0f19d1df4674e32a
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2017-08-03 17:31:46 +0200

    renaming

--- Entity/ProductReview.php
+++ Entity/ProductReview.php
@@ -3,7 +3,7 @@
 namespace Dywee\ProductCMSBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Sylius\Component\User\Model\UserInterface;
+use Dywee\CoreBundle\Model\UserInterface;
 
 /**
  * ProductReview
@@ -50,8 +50,8 @@ class ProductReview
      */
     private $moderated = false;
 
-    /*
-     * @ORM\ManyToOne(targetEntity="Dywee\UserBundle\Entity\User")
+    /**
+     * @ORM\ManyToOne(targetEntity="Dywee\CoreBundle\Model\UserInterface")
      */
     private $createdBy;
 
